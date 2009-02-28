87b6ed90cfeaefe622ff6fb18bdec1cdc78c1b0b
Siddharth Agarwal <sid.bugzilla@gmail.com>
2009-02-28 20:10:54 +0530

    Add license header, clean trailing whitespace

--- firewall-auth.py
+++ firewall-auth.py
@@ -1,5 +1,28 @@
 #!/usr/bin/python
 
+# Copyright (c) 2009 Siddharth Agarwal
+#
+# Permission is hereby granted, free of charge, to any person
+# obtaining a copy of this software and associated documentation
+# files (the "Software"), to deal in the Software without
+# restriction, including without limitation the rights to use,
+# copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the
+# Software is furnished to do so, subject to the following
+# conditions:
+#
+# The above copyright notice and this permission notice shall be
+# included in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+# OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+# NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+# HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+# WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+# FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+# OTHER DEALINGS IN THE SOFTWARE.
+
 import httplib
 import urllib
 import urlparse
@@ -14,7 +37,8 @@ def FirewallKeepAlive(url):
   # Connect to the firewall
   conn = httplib.HTTPConnection(url.netloc)
   conn.request("GET", url.path + "?" + url.query)
-  
+  conn.close()
+
   # Set a timer
   t = threading.Timer(1600.0, FirewallKeepAlive, [url])
   t.start()
@@ -30,40 +54,40 @@ def FirewallAuth(username, password):
   if (response.status == 401):
     data = response.read()
     conn.close()
-    
+
     # Look for the right magic value in the data
     match = re.search(r"VALUE=\"([0-9a-f]+)\"", data)
     magicString = match.group(1)
     logger.debug("The magic string is:", magicString)
-    
+
     # Now construct a POST request
     params = urllib.urlencode({'username': username, 'password': password,
                                'magic': magicString, '4Tredir': '/'})
     headers = {"Content-Type": "application/x-www-form-urlencoded",
                "Accept": "text/plain"}
-    
+
     postConn = httplib.HTTPConnection("www.google.com:80")
     postConn.request("POST", "/", params, headers)
-    
+
     # Get the response
     postResponse = postConn.getresponse()
-    
+
     postData = postResponse.read()
     postConn.close()
-    
+
     # Look for the keepalive URL
     keepaliveMatch = re.search(r"location.href=\"(.+?)\"", postData)
     if keepaliveMatch is None:
       # Whoops, unsuccessful -- probably the username and password didn't match
       logger.fatal("Authentication unsuccessful, check your username and password")
       return 3
-    
+
     keepaliveURL = keepaliveMatch.group(1)
-    
+
     logger.info("The keep alive URL is: " + keepaliveURL)
     logger.debug(postData)
     FirewallKeepAlive(urlparse.urlparse(keepaliveURL))
-  
+
   else:
     logger.fatal(("Server returned %d %s, so we cannot proceed. Are you " +
                  "already authenticated?") %
@@ -76,20 +100,20 @@ Main function
 def main(argv = None):
   if argv is None:
     argv = sys.argv[1:]
-  
+
   # First generate help syntax
   usage = "Usage: %prog [options] username password"
   parser = OptionParser(usage = usage)
   parser.add_option("-v", "--verbose", action = "store_true", dest = "verbose",
                     help = "Print lots of debugging information")
-  
+
   # Parse arguments
   (options, args) = parser.parse_args(argv)
-  
+
   if len(args) != 2:
     parser.error("invalid number of arguments")
     return 1
-  
+
   logger = logging.getLogger("FirewallLogger")
   logger.setLevel(logging.DEBUG)
   handler = logging.StreamHandler()
@@ -97,11 +121,11 @@ def main(argv = None):
     handler.setLevel(logging.DEBUG)
   else:
     handler.setLevel(logging.INFO)
-  
+
   formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
   handler.setFormatter(formatter)
   logger.addHandler(handler)
-  
+
   # Try authenticating!
   return FirewallAuth(args[0], args[1])
 
