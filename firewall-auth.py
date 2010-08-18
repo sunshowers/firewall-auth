6b0cae824f120e8ea49b49d8347c9a162c928177
Siddharth Agarwal <sid.bugzilla@gmail.com>
2010-08-19 04:48:00 +0530

    Rewrite everything in the form of a state machine
    --HG--
    branch : state-machine-rewrite

--- firewall-auth.py
+++ firewall-auth.py
@@ -32,73 +32,151 @@ from optparse import OptionParser
 import sys
 import logging
 import time
+import atexit
 
-def InitLogger(options):
+class FirewallState:
+  Start, LoggedIn, End = range(3)
+
+# Globals, set right in the beginning
+username = None
+password = None
+
+def start_func():
+  """
+  This is called when we're in the initial state. If we're already logged in, we
+  can't do anything much. If we're not, we should transition to the
+  not-logged-in state.
+  """
+  ERROR_RETRY_SECS = 15
+  LOGGED_IN_RETRY_SECS = 5
   logger = logging.getLogger("FirewallLogger")
-  logger.setLevel(logging.DEBUG)
-  handler = logging.StreamHandler()
-  if options.verbose:
-    handler.setLevel(logging.DEBUG)
+
+  try:
+    loginstate, data = login()
+  except httplib.HTTPException as e:
+    logger.info("Exception |%s| while trying to log in. Retrying in %d seconds." %
+                (e, ERROR_RETRY_SECS))
+    return (FirewallState.Start, ERROR_RETRY_SECS, None)
+
+  # Check whether we're logged in
+  if loginstate == LoginState.AlreadyLoggedIn:
+    logger.info("You're already logged in (response code %d). Retrying in %d seconds." %
+                (data, LOGGED_IN_RETRY_SECS))
+    return (FirewallState.Start, LOGGED_IN_RETRY_SECS, None)
+  elif loginstate == LoginState.InvalidCredentials:
+    # Not much we can do.
+    return (FirewallState.End, 0, [3])
   else:
-    handler.setLevel(logging.INFO)
+    # Yay, we logged in.
+    return (FirewallState.LoggedIn, 0, [data])
 
-  formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
-  handler.setFormatter(formatter)
-  logger.addHandler(handler)
+def logged_in_func(keepaliveurl):
+  """
+  Keep the firewall authentication alive by pinging a keepalive URL every few
+  seconds. If there are any connection problems, keep trying with the same
+  URL. If the keepalive URL doesn't work any more, go back to the start state.
+  """
+  logger = logging.getLogger("FirewallLogger")
+  ERROR_RETRY_SECS = 15
+  LOGGED_IN_SECS = 200
+  try:
+    keep_alive(keepaliveurl)
+  except httplib.BadStatusLine:
+    logger.info("The keepalive URL %s doesn't work. Attempting to log in again." %
+                keepaliveurl.geturl())
+    return (FirewallState.Start, 0, None)
+  except httplib.HTTPException as e:
+    logger.info("Exception |%s| while trying to keep alive. Retrying in %d seconds." %
+                (e, ERROR_RETRY_SECS))
+    return (FirewallState.LoggedIn, ERROR_RETRY_SECS, [keepaliveurl])
 
-def FirewallKeepAlive(url):
-  while 1:
+  # OK, the URL worked. That's good.
+  return (FirewallState.LoggedIn, LOGGED_IN_SECS, [keepaliveurl])
+
+state_functions = {
+  FirewallState.Start: start_func,
+  FirewallState.LoggedIn: logged_in_func,
+  FirewallState.End: sys.exit
+}
+
+def run_state_machine():
+  """
+  Runs the state machine defined above.
+  """
+  state = FirewallState.Start
+  args = None
+  sleeptime = 0
+  def atexit_logout():
+    """
+    Log out from firewall authentication. This is supposed to run whenever the
+    program exits.
+    """
     logger = logging.getLogger("FirewallLogger")
-    logger.info("Sending request to keep alive")
-    # Connect to the firewall
-    conn = httplib.HTTPSConnection(url.netloc)
-    conn.request("GET", url.path + "?" + url.query)
+    if state == FirewallState.LoggedIn:
+      url = args[0]
+      logouturl = urlparse.ParseResult(url.scheme, url.netloc, "/logout",
+                                       url.params, url.query, url.fragment)
+      try:
+        conn = httplib.HTTPSConnection(logouturl.netloc)
+        logger.info("Logging out with URL %s" % logouturl)
+        conn.request("GET", logouturl.path + "?" + logouturl.query)
+        response = conn.getresponse()
+        response.read()
+      except httplib.HTTPException as e:
+        # Just print an error message
+        logger.info("Exception |%s| while logging out." % e)
+      finally:
+        conn.close()
+
+  atexit.register(atexit_logout)
+
+  try:
+    while True:
+      statefunc = state_functions[state]
+      if args is None:
+        state, sleeptime, args = statefunc()
+      else:
+        state, sleeptime, args = statefunc(*args)
+      if sleeptime > 0:
+        time.sleep(sleeptime)
+  except (KeyboardInterrupt, SystemExit):
+    atexit_logout()
+
+class LoginState:
+  AlreadyLoggedIn, InvalidCredentials, Successful = range(3)
+
+def login():
+  """
+  Attempt to log in. Returns AlreadyLoggedIn if we're already logged in,
+  InvalidCredentials if the username and password given are incorrect, and
+  Successful if we have managed to log in. Throws an exception if an error
+  occurs somewhere along the process.
+  """
+  logger = logging.getLogger("FirewallLogger")
+  # Find out where to auth
+  try:
+    conn = httplib.HTTPConnection("74.125.67.100:80")
+    conn.request("GET", "/")
     response = conn.getresponse()
+    # 303 leads to the auth page, so it means we're not logged in
+    if (response.status != 303):
+      return (LoginState.AlreadyLoggedIn, response.status)
 
-    logger.debug(str(response.status))
-    logger.debug(response.read())
-
+    authlocation = response.getheader("Location")
+  finally:
     conn.close()
 
-    # Set a timer
-    time.sleep(200);
-
-"""
-This checks whether we're logged in already
-"""
-def IsLoggedIn():
-  # Connect to Google, see if we can connect or not. We use the IP directly here
-  # so that this runs on computers even if they don't have DNS configured.
-  conn = httplib.HTTPConnection("74.125.67.100:80")
-  conn.request("GET", "/")
-  response = conn.getresponse()
-  conn.close()
-  # 303 leads to the auth page, which means we aren't logged in
-  return not (response.status == 303)
-
-def FirewallAuth(username, password):
-  logger = logging.getLogger("FirewallLogger")
-
-  # If we're logged in, keep retrying until we've logged out
-  while IsLoggedIn():
-    logger.info("It seems like you're already logged in. Trying again in 5 seconds...")
-    time.sleep(5)
-
-  # At this point we aren't logged in
-  conn = httplib.HTTPConnection("74.125.67.100:80")
-  conn.request("GET", "/")
-  response = conn.getresponse()
-  authLocation = response.getheader("Location")
-  conn.close()
-  logger.info("The auth location is: " + authLocation)
+  logger.info("The auth location is: %s" % authlocation)
 
   # Make a connection to the auth location
-  parsedAuthLocation = urlparse.urlparse(authLocation)
-  authConn = httplib.HTTPSConnection(parsedAuthLocation.netloc)
-  authConn.request("GET", parsedAuthLocation.path + "?" + parsedAuthLocation.query)
-  authResponse = authConn.getresponse()
-  data = authResponse.read()
-  authConn.close()
+  parsedauthloc = urlparse.urlparse(authlocation)
+  try:
+    authconn = httplib.HTTPSConnection(parsedauthloc.netloc)
+    authconn.request("GET", parsedauthloc.path + "?" + parsedauthloc.query)
+    authResponse = authconn.getresponse()
+    data = authResponse.read()
+  finally:
+    authconn.close()
 
   # Look for the right magic value in the data
   match = re.search(r"VALUE=\"([0-9a-f]+)\"", data)
@@ -111,32 +189,52 @@ def FirewallAuth(username, password):
   headers = {"Content-Type": "application/x-www-form-urlencoded",
              "Accept": "text/plain"}
 
-  postConn = httplib.HTTPSConnection(parsedAuthLocation.netloc)
-  postConn.request("POST", "/", params, headers)
+  try: 
+    postconn = httplib.HTTPSConnection(parsedauthloc.netloc)
+    postconn.request("POST", "/", params, headers)
 
-  # Get the response
-  postResponse = postConn.getresponse()
-
-  postData = postResponse.read()
-  postConn.close()
+    # Get the response
+    postResponse = postconn.getresponse()
+    postData = postResponse.read()
+  finally:
+    postconn.close()
 
   # Look for the keepalive URL
   keepaliveMatch = re.search(r"location.href=\"(.+?)\"", postData)
   if keepaliveMatch is None:
     # Whoops, unsuccessful -- probably the username and password didn't match
-    logger.fatal("Authentication unsuccessful, check your username and password")
-    return 3
+    logger.fatal("Authentication unsuccessful, check your username and password.")
+    return (LoginState.InvalidCredentials, None)
 
   keepaliveURL = keepaliveMatch.group(1)
 
   logger.info("The keep alive URL is: " + keepaliveURL)
   logger.debug(postData)
-  FirewallKeepAlive(urlparse.urlparse(keepaliveURL))
+  return (LoginState.Successful, urlparse.urlparse(keepaliveURL))
 
-"""
-Get the username and password either from command line args or interactively
-"""
-def GetUsernameAndPassword(args):
+def keep_alive(url):
+  """
+  Attempt to keep the connection alive by pinging a URL.
+  """
+  logger = logging.getLogger("FirewallLogger")
+  logger.info("Sending request to keep alive.")
+  # Connect to the firewall
+  try:
+    conn = httplib.HTTPSConnection(url.netloc)
+    conn.request("GET", url.path + "?" + url.query)
+    # This line raises an exception if the URL stops working. We catch it in
+    # logged_in_func.
+    response = conn.getresponse()
+
+    logger.debug(str(response.status))
+    logger.debug(response.read())
+  finally:
+    conn.close()
+
+def get_credentials(args):
+  """
+  Get the username and password, either from command line args or interactively.
+  """
   username = None
   if len(args) == 0:
     # Get the username from the input
@@ -154,6 +252,19 @@ def GetUsernameAndPassword(args):
     password = args[1]
 
   return (username, password)
+
+def init_logger(options):
+  logger = logging.getLogger("FirewallLogger")
+  logger.setLevel(logging.DEBUG)
+  handler = logging.StreamHandler()
+  if options.verbose:
+    handler.setLevel(logging.DEBUG)
+  else:
+    handler.setLevel(logging.INFO)
+
+  formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
+  handler.setFormatter(formatter)
+  logger.addHandler(handler)
 
 """
 Main function
@@ -175,11 +286,13 @@ def main(argv = None):
     parser.error("too many arguments")
     return 1
 
-  InitLogger(options)
+  init_logger(options)
 
   # Try authenticating!
-  (username, password) = GetUsernameAndPassword(args)
-  return FirewallAuth(username, password)
+  global username, password
+  username, password = get_credentials(args)
+  run_state_machine()
+  return 0
 
 if __name__ == "__main__":
   sys.exit(main())
